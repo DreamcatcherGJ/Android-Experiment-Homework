7f61187948b6f70a024be71bcb713e1940801cae
Admin <Greamcatchergj@gmail.com>
2020-06-03 17:21:20 +0800

    GJ

---
+++ app/src/main/java/homework/taohuo/GX/ListFragment.java
@@ -0,0 +1,29 @@
+package homework.taohuo.GX;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import homework.taohuo.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ListFragment extends Fragment {
+
+    public ListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list, container, false);
+    }
+}
