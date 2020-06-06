8ee33b50d1e3fa8854609cdf920445543d42a1a6
happy丿虎 <1913673745@qq.com>
2020-06-06 18:52:32 +0800

    ZH

---
+++ app/src/main/java/homework/taohuo/ZH/HomeFragment.java
@@ -0,0 +1,218 @@
+package homework.taohuo.ZH;
+import homework.taohuo.R;
+
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.annotation.Nullable;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import android.view.LayoutInflater;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Administrator on 2016/7/13.
+ */
+public class HomeFragment extends Fragment {
+
+    private View mView;
+    private ViewPager mViewPaper;
+    private List<ImageView> images;
+    private List<View> dots;
+    private int currentItem;
+    private Button btn1,btn2,btn3,btn4,btn5;
+    //记录上一次点的位置
+    private int oldPosition = 0;
+    //存放图片的id
+    private int[] imageIds = new int[]{
+            R.mipmap.zh_img01,
+            R.mipmap.zh_img02,
+            R.mipmap.zh_img03,
+            R.mipmap.zh_img04,
+            R.mipmap.zh_img05
+    };
+    //存放图片的标题
+    /*private String[] titles = new String[]{
+            "轮播1",
+            "轮播2",
+            "轮播3",
+            "轮播4",
+            "轮播5"
+    };*/
+    private TextView title;
+    private ViewPagerAdapter adapter;
+    private ScheduledExecutorService scheduledExecutorService;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        mView = inflater.inflate(R.layout.fragment_home, null);
+        setView();
+        return mView;
+    }
+
+    private void setView() {
+        mViewPaper = (ViewPager) mView.findViewById(R.id.vp);
+
+        //显示的图片
+        images = new ArrayList<ImageView>();
+        for (int i = 0; i < imageIds.length; i++) {
+            ImageView imageView = new ImageView(getActivity());
+            imageView.setBackgroundResource(imageIds[i]);
+            images.add(imageView);
+        }
+        //显示的小点
+        dots = new ArrayList<View>();
+        dots.add(mView.findViewById(R.id.dot_0));
+        dots.add(mView.findViewById(R.id.dot_1));
+        dots.add(mView.findViewById(R.id.dot_2));
+        dots.add(mView.findViewById(R.id.dot_3));
+        dots.add(mView.findViewById(R.id.dot_4));
+
+        //title = (TextView) mView.findViewById(R.id.title);
+        //title.setText(titles[0]);
+
+        adapter = new ViewPagerAdapter();
+        mViewPaper.setAdapter(adapter);
+
+        mViewPaper.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+
+            @Override
+            public void onPageSelected(int position) {
+                //title.setText(titles[position]);
+                dots.get(position).setBackgroundResource(R.mipmap.zh_dot_yes);
+                dots.get(oldPosition).setBackgroundResource(R.mipmap.zh_dot_no);
+
+                oldPosition = position;
+                currentItem = position;
+            }
+
+            @Override
+            public void onPageScrolled(int arg0, float arg1, int arg2) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int arg0) {
+
+            }
+        });
+    }
+
+    /*定义的适配器*/
+    public class ViewPagerAdapter extends PagerAdapter {
+
+        @Override
+        public int getCount() {
+            return images.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View arg0, Object arg1) {
+            return arg0 == arg1;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup view, int position, Object object) {
+            // TODO Auto-generated method stub
+//          super.destroyItem(container, position, object);
+//          view.removeView(view.getChildAt(position));
+//          view.removeViewAt(position);
+            view.removeView(images.get(position));
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup view, int position) {
+            // TODO Auto-generated method stub
+            view.addView(images.get(position));
+            return images.get(position);
+        }
+
+    }
+
+    /**
+     * 利用线程池定时执行动画轮播
+     */
+    @Override
+    public void onStart() {
+        // TODO Auto-generated method stub
+        super.onStart();
+        scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+        scheduledExecutorService.scheduleWithFixedDelay(
+                new ViewPageTask(),
+                2,
+                2,
+                TimeUnit.SECONDS);
+    }
+
+
+    /**
+     * 图片轮播任务
+     *
+     * @author liuyazhuang
+     */
+    private class ViewPageTask implements Runnable {
+
+        @Override
+        public void run() {
+            currentItem = (currentItem + 1) % imageIds.length;
+            mHandler.sendEmptyMessage(0);
+        }
+    }
+
+    /**
+     * 接收子线程传递过来的数据
+     */
+    private Handler mHandler = new Handler() {
+        public void handleMessage(android.os.Message msg) {
+            mViewPaper.setCurrentItem(currentItem);
+        }
+
+        ;
+    };
+
+    @Override
+    public void onStop() {
+        // TODO Auto-generated method stub
+        super.onStop();
+        if (scheduledExecutorService != null) {
+            scheduledExecutorService.shutdown();
+            scheduledExecutorService = null;
+        }
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        // final User user ;
+        btn1 = (Button) getActivity().findViewById(R.id.zh_btn1);
+        btn2 = (Button) getActivity().findViewById(R.id.zh_btn2);
+        btn3 = (Button) getActivity().findViewById(R.id.zh_btn3);
+        btn4 = (Button) getActivity().findViewById(R.id.zh_btn4);
+        btn5 = (Button) getActivity().findViewById(R.id.zh_btn5);
+        //img1 =(ImageView) getActivity().findViewById(R.id.zh_img1);
+        /*btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (blankFragment == null) {
+                    blankFragment = new BlankFragment();
+                }
+                getFragmentManager().beginTransaction().replace(R.id.fragment_container, blankFragment).commitAllowingStateLoss();
+            }
+        });*/
+    }
+}
