ce323f9749f5cec5bf180c12b8e1d7bab7354390
Administrator <379391871@qq.com>
2020-06-07 20:29:01 +0800

    WZT

--- app/src/main/java/homework/taohuo/WZT/DeleteAddress.java
+++ app/src/main/java/homework/taohuo/WZT/DeleteAddress.java
@@ -8,18 +8,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 
 import homework.taohuo.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link Addaddress#newInstance} factory method to
+ * Use the {@link DeleteAddress#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Addaddress extends Fragment {
-    private Button btn1, btn2;
-    private EditText ed1;
+public class DeleteAddress extends Fragment {
+    private Button btn1,btn2;
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -29,7 +28,7 @@ public class Addaddress extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public Addaddress() {
+    public DeleteAddress() {
         // Required empty public constructor
     }
 
@@ -39,11 +38,11 @@ public class Addaddress extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Addaddress.
+     * @return A new instance of fragment DeleteAddress.
      */
     // TODO: Rename and change types and number of parameters
-    public static Addaddress newInstance(String param1, String param2) {
-        Addaddress fragment = new Addaddress();
+    public static DeleteAddress newInstance(String param1, String param2) {
+        DeleteAddress fragment = new DeleteAddress();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -64,13 +63,13 @@ public class Addaddress extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_addaddress, container, false);
+        return inflater.inflate(R.layout.fragment_delete_address, container, false);
     }
-
     public void onActivityCreated(Bundle savedInstanceState) {
+
         super.onActivityCreated(savedInstanceState);
         btn1 = (Button) getActivity().findViewById(R.id.button1);
         btn2 = (Button) getActivity().findViewById(R.id.button2);
-        ed1  = (EditText)getActivity().findViewById(R.id.editTextTextPersonName2);
+
     }
 }
--- app/src/main/java/homework/taohuo/WZT/ModifyAddress.java
+++ app/src/main/java/homework/taohuo/WZT/ModifyAddress.java
@@ -14,12 +14,13 @@ import homework.taohuo.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link Addaddress#newInstance} factory method to
+ * Use the {@link ModifyAddress#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Addaddress extends Fragment {
-    private Button btn1, btn2;
+public class ModifyAddress extends Fragment {
+    private Button btn1,btn2;
     private EditText ed1;
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -29,7 +30,7 @@ public class Addaddress extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public Addaddress() {
+    public ModifyAddress() {
         // Required empty public constructor
     }
 
@@ -39,11 +40,11 @@ public class Addaddress extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Addaddress.
+     * @return A new instance of fragment ModifyAddress.
      */
     // TODO: Rename and change types and number of parameters
-    public static Addaddress newInstance(String param1, String param2) {
-        Addaddress fragment = new Addaddress();
+    public static ModifyAddress newInstance(String param1, String param2) {
+        ModifyAddress fragment = new ModifyAddress();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -64,13 +65,15 @@ public class Addaddress extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_addaddress, container, false);
+        return inflater.inflate(R.layout.fragment_modify_address, container, false);
     }
 
     public void onActivityCreated(Bundle savedInstanceState) {
+
         super.onActivityCreated(savedInstanceState);
         btn1 = (Button) getActivity().findViewById(R.id.button1);
         btn2 = (Button) getActivity().findViewById(R.id.button2);
         ed1  = (EditText)getActivity().findViewById(R.id.editTextTextPersonName2);
+
     }
 }
